b1c747830ccc7461f256f390fc5e233394a14290
Jose Salas <josephsalas.developer@gmail.com>
2021-09-13 19:50:08 -0500

    :alien: add util test

---
+++ tests/test_utils/test_node.py
@@ -0,0 +1,30 @@
+from api.utils.node import build_path_from_params
+from api.utils.node import paths_with_slash
+
+
+def test_build_path_from_params_with_uuid():
+    params_with_uuid = ("users", "667605b0-8c34-4dab-8c8e-c7aef7e05bc4")
+    url_original, url_modified, _uuid = build_path_from_params(params_with_uuid)
+    assert url_original == "/users/667605b0-8c34-4dab-8c8e-c7aef7e05bc4"
+    assert url_modified == "/users"
+    assert _uuid == "667605b0-8c34-4dab-8c8e-c7aef7e05bc4"
+
+
+def test_build_path_from_params_without_uuid():
+    params_without_uuid = ("users",)
+    url_original, url_modified, _uuid = build_path_from_params(params_without_uuid)
+    assert url_original == "/users"
+    assert url_modified == "/users"
+    assert _uuid == None
+
+
+def test_paths_with_slash():
+    path = "/users"
+    result = paths_with_slash(path)
+    assert result == ["/users", "/users/"]
+
+
+def test_paths_without_slash():
+    path = "/users/"
+    result = paths_with_slash(path)
+    assert result == ["/users/", "/users"]
