2d9787c78e8cd711d270e6c0f1748ac8a274cbb9
Jose Salas <josephsalas.developer@gmail.com>
2022-03-05 22:34:58 -0500

    fix tests

--- tests/test_validators/test_base.py
+++ tests/test_validators/test_base.py
@@ -6,7 +6,7 @@ from tests.conftest import AsyncMock
 
 
 @pytest.mark.asyncio
-async def test_validator_mixin_in_node_method_with_model_already_exists(mocker):
+async def test_validator_mixin_in_model_method_with_model_already_exists(mocker):
     mock_repo = mocker.patch(
         "api.repositories.repository.count", new_callable=AsyncMock
     )
@@ -15,11 +15,11 @@ async def test_validator_mixin_in_node_method_with_model_already_exists(mocker):
     v = ValidatorMixin()
 
     with pytest.raises(HTTPException):
-        await v.node("users", "/users")
+        await v.model("users", "/users")
 
 
 @pytest.mark.asyncio
-async def test_validator_mixin_in_node_method_with_path_already_exists(mocker):
+async def test_validator_mixin_in_model_method_with_path_already_exists(mocker):
     mock_repo = mocker.patch(
         "api.repositories.repository.count", new_callable=AsyncMock
     )
@@ -28,11 +28,11 @@ async def test_validator_mixin_in_node_method_with_path_already_exists(mocker):
     v = ValidatorMixin()
 
     with pytest.raises(HTTPException):
-        await v.node("users", "/users")
+        await v.model("users", "/users")
 
 
 @pytest.mark.asyncio
-async def test_validator_mixin_in_node_method_with_validation_ok(mocker):
+async def test_validator_mixin_in_model_method_with_validation_ok(mocker):
     mock_repo = mocker.patch(
         "api.repositories.repository.count", new_callable=AsyncMock
     )
@@ -40,5 +40,5 @@ async def test_validator_mixin_in_node_method_with_validation_ok(mocker):
     mock_repo.side_effect = [0, 0]
     v = ValidatorMixin()
 
-    expect = await v.node("users", "/users")
+    expect = await v.model("users", "/users")
     assert expect is None
