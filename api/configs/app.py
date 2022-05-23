b79a30dc11be40a2038d01aa8846394388e2a028
Jose Salas <josephsalas.developer@gmail.com>
2022-05-23 16:33:54 -0500

    :bookmark: origins

--- api/configs/app.py
+++ api/configs/app.py
@@ -5,10 +5,12 @@ ENGINE_URI_DEFAULT = "mongodb://0.0.0.0:27017/"
 ENGINE_NAME = os.environ.get("ENGINE_NAME", "MONGO")
 ENGINE_DB_NAME = os.environ.get("ENGINE_DB_NAME", "apisrun")
 ENGINE_URI = os.environ.get("ENGINE_URI", ENGINE_URI_DEFAULT)
-ORIGINS = [
+ORIGINS_DEFAULT = [
     "http://localhost",
     "http://localhost:8080",
 ]
+ORIGINS_PROD = os.environ.get("ORIGINS")
+ORIGINS = ORIGINS_PROD.split(",") if ORIGINS_PROD else ORIGINS_DEFAULT
 
 # Validator configs
 VALIDATOR_NAME = "CERBERUS"
